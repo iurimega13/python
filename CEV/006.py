9acfbc7b55e92816fe3451675f0f1ca2b908e329
iurimega13 <iurimega13@gmail.com>
2021-08-18 11:31:08 -0300

    Código 006

---
+++ CEV/006.py
@@ -0,0 +1,7 @@
+x= int(input('Digite um número: '))
+print('Seu número é: {0}\nSeu dobro é: {1}\nSeu triplo é: {2}\nSua raiz quadrada é: {3}'.format(
+    x,
+    (x * 2),
+    (x * 3),
+    pow(x,0.5)
+))
