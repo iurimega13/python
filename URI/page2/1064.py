d15b3ea6be3bea93f257e7f4a3d2dcd30367ca93
iurimega13 <iurimega13@gmail.com>
2021-08-24 20:54:58 -0300

    Código 1064

---
+++ URI/page2/1064.py
@@ -0,0 +1,24 @@
+"""
+Leia 6 valores. Em seguida, mostre quantos destes valores digitados foram positivos. Na próxima linha, deve-se mostrar a média de todos os valores positivos digitados, com um dígito após o ponto decimal.
+Entrada
+A entrada contém 6 números que podem ser valores inteiros ou de ponto flutuante. Pelo menos um destes números será positivo.
+Saída
+O primeiro valor de saída é a quantidade de valores positivos. A próxima linha deve mostrar a média dos valores positivos digitados.
+7
+-5
+6
+-3.4
+4.6
+12
+4 valores positivos
+7.4
+"""
+positivos = 0
+media = 0
+for i in range(6):
+    n = float(input())
+    if n > 0:
+        positivos += 1
+        media += n
+print('{} valores positivos'.format(positivos))
+print('{:.1f}'.format(media/positivos))
