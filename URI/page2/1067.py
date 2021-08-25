220b31ab260ced824fdd18bd70a769e311afbe94
iurimega13 <iurimega13@gmail.com>
2021-08-24 21:24:00 -0300

    Código 1067

---
+++ URI/page2/1067.py
@@ -0,0 +1,21 @@
+"""
+Leia um valor inteiro X (1 <= X <= 1000). Em seguida mostre os ímpares de 1 até X, um valor por linha, inclusive o X, se for o caso.
+
+Entrada
+O arquivo de entrada contém 1 valor inteiro qualquer.
+
+Saída
+Imprima todos os valores ímpares de 1 até X, inclusive X, se for o caso.
+
+Exemplo de Entrada	Exemplo de Saída
+8
+
+1
+3
+5
+7
+"""
+
+for i in range(int(input())+1):
+    if i % 2 != 0:
+        print(i)
