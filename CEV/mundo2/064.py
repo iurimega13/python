13e543a277eee995169677c98e06b02803f0ff37
iurimega13 <iurimega13@gmail.com>
2021-09-07 11:39:35 -0300

    Código 064

---
+++ CEV/mundo2/064.py
@@ -0,0 +1,13 @@
+"""
+Exercício Python 64: Crie um programa que leia vários números inteiros pelo teclado. O programa só vai parar quando o usuário digitar o valor 999, que é a condição de parada. No final, mostre quantos números foram digitados e qual foi a soma entre eles (desconsiderando o flag).
+"""
+soma = 0
+qtd = 0
+while True:
+    n = int(input("Digite um número [999 para parar]: "))
+    if n == 999:
+        break
+    else:
+        soma += n
+        qtd += 1
+print("Você digitou {} números e a soma entre eles é {}.".format(qtd, soma))
